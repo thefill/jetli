158407c05d68713605c9326e0fa8086920166e39
Filip Dabrowski <fifofil@gmail.com>
2019-05-21 21:32:11 +0100

    Change way service is exported, update docs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 // Export classes and interfaces just in case
 export * from './interfaces';
 export * from './classes';
-
-// make service exported as default for the easiness of usage
-import {jetli} from './services';
-export default jetli;
+export * from './services';
